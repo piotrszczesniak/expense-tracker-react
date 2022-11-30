b4f313a9f3a635dace2b66d54a3582ab547d0c08
Piotr Szcześniak <piotr.szcz3sniak@gmail.com>
2022-11-30 21:50:11 +0100

    handle inputs and selects with states, add handle functions

--- src/components/TransactionsInput/TransactionInputs.js
+++ src/components/TransactionsInput/TransactionInputs.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './TransactionInputs.module.scss';
 
-export const TransactionInputs = ({ onTransactionAdded, currency }) => {
+export const TransactionInputs = ({ onTransactionAdded }) => {
+  const [transaction, setTransaction] = useState({
+    id: '',
+    name: '',
+    amount: '',
+    type: 'cost',
+    currency: 'PLN',
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -13,10 +21,10 @@ export const TransactionInputs = ({ onTransactionAdded, currency }) => {
         },
         body: JSON.stringify({
           id: new Date().getTime(),
-          name: e.target[0].value,
-          amount: e.target[1].value,
-          type: e.target[2].value,
-          currency: currency,
+          name: transaction.name,
+          amount: transaction.amount,
+          type: transaction.type,
+          currency: transaction.currency,
         }),
       });
       const data = await response.json();
@@ -33,7 +41,6 @@ export const TransactionInputs = ({ onTransactionAdded, currency }) => {
     //     amount: e.target[1].value,
     //     type: e.target[2].value,
     //   },
-
     // ]);
 
     // e.target[1].value = ''
@@ -42,33 +49,64 @@ export const TransactionInputs = ({ onTransactionAdded, currency }) => {
     // formik
   };
 
+  const handleNameChange = (e) => {
+    setTransaction((transaction) => ({ ...transaction, name: e.target.value }));
+  };
+
+  const handleAmountChange = (e) => {
+    setTransaction((transaction) => ({ ...transaction, amount: e.target.value }));
+  };
+
+  const handleTypeChange = (e) => {
+    setTransaction((transaction) => ({ ...transaction, type: e.target.value }));
+  };
+
+  const handleCurrencyChange = (e) => {
+    setTransaction((transation) => ({ ...transation, currency: e.target.value }));
+  };
+
   return (
     <section className={'transaction-inputs'}>
       <h3 className={styles['transaction-title']}>Add new transaction</h3>
 
       <form className={styles['transaction-form']} onSubmit={handleSubmit}>
         <label className={styles['label']}>
-          Enter name of transaction:
-          <input className={styles['input-text']} required type='text' placeholder='Name' />
+          Enter name of transaction: {transaction.name}
+          <input
+            className={styles['input-text']}
+            required
+            type='text'
+            placeholder='Name'
+            value={transaction.name}
+            onChange={handleNameChange}
+          />
         </label>
         <label className={styles.label}>
           Enter amount of transaction:
-          <input className={styles['input-text']} required type='number' step='0.01' placeholder='Amount' />
+          <input
+            className={styles['input-text']}
+            required
+            type='number'
+            step='0.01'
+            placeholder='Amount'
+            value={transaction.amount}
+            onChange={handleAmountChange}
+          />
         </label>
         <label className={styles.label}>
           Select type of transaction:
-          <select className={styles['input-text']}>
+          <select className={styles['input-text']} value={transaction.type} onChange={handleTypeChange}>
             <option value='cost'>Cost</option>
             <option value='income'>Income</option>
           </select>
         </label>
         <label className={styles.label}>
           Select currency of transaction:
-          <select className={styles['input-text']}>
-            <option value='PLN'>PLN</option>
+          <select className={styles['input-text']} value={transaction.currency} onChange={handleCurrencyChange}>
             <option value='EUR'>EUR</option>
             <option value='GBP'>GBP</option>
             <option value='USD'>USD</option>
+            <option value='PLN'>PLN</option>
           </select>
         </label>
         <button className={styles['input-submit']} type='submit'>
